af45e6bc13cbd5ffa19e5774076310bfd4d1516e
Quang Khuat <kdquang123@gmail.com>
2023-10-05 23:28:27 +0700

    init

---
+++ Models/Course.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lab4.Models
+{
+    public class Course
+    {
+        public Course() { 
+            Enrollments = new HashSet<Enrollment>();
+        }
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+
+        public string Title { get; set; }
+        public int Credits { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
---
+++ Models/Major.cs
@@ -0,0 +1,15 @@
+﻿namespace Lab4.Models
+{
+    public class Major
+    {
+
+        public Major()
+        {
+            Learners=new HashSet<Learner>();
+        }
+        public int MajorID { get; set; }
+        public string MajorName { get; set; }
+        public ICollection<Learner> Learners { get; set; }
+
+    }
+}
